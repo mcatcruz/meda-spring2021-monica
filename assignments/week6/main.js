5f865e57062350771c67bac871a25510c9845adf
Monica C <mcatcruz@gmail.com>
2021-02-25 22:25:54 -0800

    Worked on work.html, contact.html, and index.html. Updated design_brief.txt. Also updated main.js in assignments folder.

--- assignments/week6/main.js
+++ assignments/week6/main.js
@@ -161,13 +161,172 @@ if (myFirstObject.firstName === 'Monica') {
     console.log('Welcome!');
 }
 
-var numberToTest = -15;
+var numberToTest = 0;
 if (numberToTest > 0) {
     console.log('the number is positive.');
+} else {
+    console.log('the number is NOT positive.');
 }
+
 if (numberToTest === 0) {
     console.log('the number is zero.');
+} else {
+    console.log('the number is not a zero.');
 }
+
+
 if (numberToTest < 0) {
     console.log('the number is negative.');
+} else {
+    console.log('the number is NOT negative.');
+}
+
+var personAge;
+
+personAge = 13;
+personLikesDrinking = true;
+
+if (personAge >= 21 && personAge <= 25) {
+    console.log('chug, chug, chug!');
+    if (personLikesDrinking == true) {
+        console.log('what do you want to drink?');
+    }
+} else if (personAge >= 26 && personAge <= 40) {
+    console.log('let\'s go drink!');
+} else {
+    console.log('have you tried knitting?');
+}
+
+// While loops
+
+var keepRunning = 0;
+
+while (keepRunning < 10) {
+    console.log('we ran the loop once!');
+    keepRunning = keepRunning + 1;
+    var count = 0;
+    while (count < 10) {
+        console.log('inner loop ran!');
+        count = count + 1;
+    
+    }
+}
+
+//Do while loop - will always run the code block at least once before checking the condition
+
+var personAnswer = 'no';
+
+do {
+    console.log('run it back!');
+    personAnswer = prompt('keep going?');
+} while (personAnswer === 'yes');
+
+//For loop, "condition" area has THREE parts
+// syntax: for (counter; condition; iterator) {}
+
+for (var keepRunning = 0; keepRunning < 10; keepRunning = keepRunning + 1) {
+    console.log('this for loop ran once!');
+}
+
+for (var i = 0; i < 10; i++) {
+    if (valueFound == true) {
+        break;
+    }
+}
+
+// ++ = increment by one; same as i = i + 1
+// -- = decrement by one
+
+//break vs. continue - for when you find what you're looking for early. 
+// break - exits the loop;
+// continue - skips the specific iteration, but the loop keeps going
+
+// .forEach() - a function for arrays that goes through each element of the array and does something to each element.
+
+var someArray = [10, 20, 30, 40, 50]
+
+someArray.forEach(function (arrayValue) {
+    console.log(arrayValue + 100);
+}); //expected output: 110, 120, 130, 140, 150
+
+
+
+//Functions 
+
+function myFirstFunction() {
+    console.log('hello from first function!');
+}
+
+myFirstFunction(); //expected output: 'hello from first function!'
+
+
+function addTwoNumbers(num1, num2) {
+    var total = num1 + num2;
+    console.log(total);
+}
+
+
+// Arguments are written winthin the parentheses of the CALL.
+
+addTwoNumbers(10, 1);
+addTwoNumbers(100, 5);
+addTwoNumbers(-5, 10);
+
+// Returning values from Functions
+
+// Concatenation 
+
+function makeExciting(sentence) {
+    var excitingSentence = sentence + "!!!";
+    console.log(excitingSentence);
+    return excitingSentence;
+}
+
+var savedSentence = makeExciting('How are you doing');
+var savedSentence2 = makeExciting('Would you like ice cream');
+
+// Example Function
+
+function mathSummarizer(num1, num2) {
+    var sum = num1 + num2;
+    var difference = num1 - num2;
+    var differenceReverse = num2 - num1;
+    var quotient = num1 / num2;
+    var quotientReversed = num2/num1;
+    var product = num1 * num2;
+
+    var object = {
+        "Sum": sum,
+        "Difference": difference,
+        "Difference Reverse": differenceReverse,
+        "Quotient": quotient,
+        "Quotient Reversed": quotientReversed,
+        "Product": product
+    };
+
+    return object;
+
+var number1 = 9;
+var number2 = 15;
+var results = mathSummarizer(number1, number2);
+
+console.log(results);
+
+var sumSentence = "The sum of 9 and 15 is " + results["Sum"];
+console.log(sumSentence);
+
+
+//expected output: 
+// console.log(mathSummarizer(0, 0)); quotient and quotientReversed will result in NaN
+
+}
+
+function mathForMe(num1, num2) {
+ 
+    var results = mathSummarizer(num1, num2);
+
+    for (var keyName in results) {
+        console.log("The " + keyName + " of " + num1 + " and " + num2 + " is " + results[keyName]);
+    }
+    
 }
